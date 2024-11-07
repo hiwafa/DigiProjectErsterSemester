29c719f80b5dfdee651fcba5fc98afb9fe4c6e9b
hiwafa <mq.func@gmail.com>
2024-11-08 00:05:26 +0100

    New Update

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,11 +1,20 @@
 from utils import format_message
 from responses import get_welcome_message, get_opening_question, handle_input
 
+
 def main():
     print(format_message("Chatbot", get_welcome_message()))
     print(format_message("Chatbot", get_opening_question()))
 
     while True:
-        user_input = input(format_message("User", "")).strip()
+        user_input = input(format_message("Benutzer", "")).strip()
         response = handle_input(user_input)
-        
+
+        if response is None:
+            print(format_message("Chatbot", "Tschüss!"))
+            break
+        print(format_message("Chatbot", response))
+
+
+if __name__ == "__main__":
+    main()
--- chatbot/responses.py
+++ chatbot/responses.py
@@ -4,7 +4,18 @@ def get_welcome_message():
 def get_opening_question():
     return "Wie kann ich Ihnen behilflich sein?"
 
+
+predefined_answers = {
+    "Wie heißen Sie?": "Ich bin Chatbot.",
+    "Wie funktioniert diese App?": "Geben Sie Ihre Frage ein und ich werde versuchen, sie zu beantworten."
+}
+
+
 def handle_input(user_input):
     if user_input.lower() == "Tschüss":
         return None  # Das Gespräch zu Ende zu bringen
-    return f"Sie habe gesagt: {user_input}"
+    elif user_input in predefined_answers:
+        return predefined_answers[user_input]
+    # return f"Sie habe gesagt: {user_input}"
+    else:
+        return "Es tut mir leid, ich kenne diese Frage nicht. Bitte Stelle Sie eine andere Frage."
